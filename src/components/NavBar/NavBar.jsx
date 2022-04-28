75d1eb497eb954a298467f8ce8bbec4a3d74f031
marinkarokh <95945562+marinkarokh@users.noreply.github.com>
2022-04-28 03:54:23 +0300

    замена названия в шапке
    з

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -131,7 +131,7 @@ const NavBar = () => {
             component="div"
             sx={{ mr: 2, display: { xs: "none", md: "flex" } }}
           >
-            FINAL PROJECT TEAM #6
+            CULTURENET
           </Typography>
 
          
